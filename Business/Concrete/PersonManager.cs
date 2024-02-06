57aed431e7dfb1a574cf080a64d9a0ad83335bb0
Esad <mesadkar@gmail.com>
2024-02-06 16:11:46 +0300

    deneme

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entities.Concrete;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PersonManager
+{
+    public void ApplyForMask(Person person)
+    {
+
+    }
+}
--- odev1/Program.cs
+++ odev1/Program.cs
@@ -1,1 +1,16 @@
 ﻿
+Vatandas vatandas1 = new Vatandas();
+vatandas1.Ad = "E";
+SelamVer();
+
+static void SelamVer()
+{
+    Console.WriteLine("Selam");
+}
+public class Vatandas
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public int DogumYili { get; set; }
+    public long TcNo { get; set; }
+}
